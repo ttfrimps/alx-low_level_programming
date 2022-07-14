06faf2faec526f99453668d8161f23ffec0efb43
ttfrimps <theocole167@gmail.com>
2022-07-14 08:43:05 -0700

    task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncat - function to append some charx
+ * @dest: first param
+ * @src: second param
+ * @n: third param
+ * Return: string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	strncat(dest, src, n);
+	return (dest);
+}
